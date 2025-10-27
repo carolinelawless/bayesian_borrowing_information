db4e9c3ea0fed058c8121f418bd1aeaa029ef7cd
carolinelawless <carolinelawless5@gmail.com>
2025-10-27 10:30:15 +0100

    changes

--- TEA.R
+++ TEA.R
@@ -1,7 +1,8 @@
 remove(list = ls())
 
+
 M <- 10 #number of particles
-epsilon <- 0.5
+
 
 v1 <- rnorm(5, 1, 1)
 v2 <- rnorm(5, 2, 1)
@@ -10,6 +11,7 @@ v4 <- rnorm(5, 4, 1)
 v5 <- rnorm(5, 5, 1)
 V <- rbind(v1, v2, v3, v4, v5)
 K <- nrow(V)
+epsilon <- rep(0.5, K)
 
 k = 0
 mu  <- runif(M, 0, 5)
@@ -17,26 +19,34 @@ U <- rep(0, M)
 mu_mat <- mu
 U_mat <- U
 
-k <- k+1
-U <- rbinom(M, 1, epsilon)
+while(k < K){
+  k <- k+1
+  U <- rbinom(M, 1, epsilon[k])
+  
+  mu2 <- runif(length(which(U == 0)), 0, 5)
+  mu[which(U == 0)] <- mu2
+  data <- V[k,]
+  w <- vector(length = length(mu))
+  for(i in 1:length(mu)){
+    likelihoods <- dnorm(data, mu[i], 1)
+    w[i] <- prod(likelihoods)
+  }
+  s <- sum(w)
+  w <- w/s
+  ESS <- 1/sum(w^2)
+  mu_mat <- rbind(mu_mat, mu)
+  U_mat <- rbind(U_mat, U)
+  if(ESS < M/2){
+    samp <- sample(1:M, M, replace = TRUE, prob = w)
+    mu_mat <- mu_mat[,samp]
+    U_mat <- U_mat[, samp]
+    mu <- mu[samp]
+    U <- U[samp]
+  }
+  
+}
 
-mu2 <- runif(length(which(U == 0)), 0, 5)
-mu[which(U == 0)] <- mu2
-data <- V[k,]
-w <- vector(length = length(mu1))
-for(i in 1:length(mu1)){
-  likelihoods <- dnorm(data, mu1[i], 1)
-  w[i] <- prod(likelihoods)
-}
-s <- sum(w)
-w <- w/s
-ESS <- 1/sum(w^2)
-mu_mat <- rbind(mu_mat, mu1)
-U_mat <- rbind(U_mat, U)
-if(ESS < M/2){
-  samp <- sample(1:M, M, replace = TRUE, prob = w)
-  mu_mat <- mu[,samp]
-  U_mat <- U[, samp]
-}
+U_mat
+mu_mat
 
 
