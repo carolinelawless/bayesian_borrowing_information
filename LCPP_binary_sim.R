34b4ae2b3c1c4d25d4c2dd2f96d16d271cd3379f
carolinelawless <carolinelawless5@gmail.com>
2025-11-05 16:59:42 +0100

    updates

--- LCPP_binary_sim.R
+++ LCPP_binary_sim.R
@@ -31,41 +31,41 @@ run_commensurate_model <- function(p_hist, p_current, n = 10,
       J = length(p_hist)
     )
     
-    # --- JAGS model ---
     model_string <- "
-    model {
-      # Likelihood
-      for (j in 1:J) {
-        for (i in 1:n) {
-          y_hist[j, i] ~ dbern(p_hist[j])
-        }
-      }
-      for (i in 1:n) {
-        y_current[i] ~ dbern(p_current)
-      }
-
-      # Logistic link
-      for (j in 1:J) {
-        logit(p_hist[j]) <- theta_hist[j]
-      }
-      logit(p_current) <- theta_current
-
-      # Markov structure over theta
-      theta_hist[1] ~ dnorm(0, 0.01)      # diffuse prior for first version
-      for (j in 2:J) {
-        theta_hist[j] ~ dnorm(theta_hist[j-1], tau_theta)
-      }
-
-      # Current version depends on last historical theta
-      theta_current ~ dnorm(theta_hist[J], tau_current)
-
-      # Priors for precisions (hyperparameters)
-      tau_theta <- pow(sigma_theta, -2)
-      tau_current <- pow(sigma_current, -2)
-      sigma_theta ~ dunif(0, 5)
-      sigma_current ~ dunif(0, 5)
+model {
+  # Likelihood
+  for (j in 1:J) {
+    for (i in 1:n) {
+      y_hist[j, i] ~ dbern(p_hist[j])
     }
-    "
+  }
+  for (i in 1:n) {
+    y_current[i] ~ dbern(p_current)
+  }
+
+  # Logistic link
+  for (j in 1:J) {
+    logit(p_hist[j]) <- theta_hist[j]
+  }
+  logit(p_current) <- theta_current
+
+  # Markov structure over theta
+  theta_hist[1] ~ dnorm(0, 0.01)      # diffuse prior for first version
+  
+  # Each transition can have its own precision tau_theta[j]
+  for (j in 2:J) {
+    theta_hist[j] ~ dnorm(theta_hist[j-1], tau_theta[j-1])
+    tau_theta[j-1] <- pow(sigma_theta[j-1], -2)
+    sigma_theta[j-1] ~ dunif(0, 5)
+  }
+
+  # Current version depends on last historical theta
+  theta_current ~ dnorm(theta_hist[J], tau_current)
+  tau_current <- pow(sigma_current, -2)
+  sigma_current ~ dunif(0, 5)
+}
+"
+  
     
     # --- Run model ---
     model <- jags.model(textConnection(model_string), data = data_jags,
