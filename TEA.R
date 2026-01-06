9de8799c63e5d6b94bd063585c8b1d748213654f
carolinelawless <carolinelawless5@gmail.com>
2026-01-06 15:48:50 +0100

    updates

--- TEA.R
+++ TEA.R
@@ -10,10 +10,10 @@ source("TEA_functions.R")
 
 lambda <- 1
 M <- 1000 #number of particles
-a <- 100#beta prior
-b <- 100 #beta prior
+a <- 1#beta prior
+b <- 1 #beta prior
 J <- 5 #number of versions
-n <- rep(1e2, 5) #number of observations per version
+n <- rep(1e1, 5) #number of observations per version
 nsim <- 1e3 #number of simulations for Monte Carlo S estimate
 
 
