bdfedadf594a7244ebb18e9f08545403dbb949dd
carolinelawless <carolinelawless5@gmail.com>
2026-08-17 16:44:56 +0200

    updates

--- sim9.R
+++ sim9.R
@@ -10,15 +10,15 @@ M <- 100
 #B <- 20000
 B <- 50000
 
-n_versions <- 10
+n_versions <- 20
 
 a_theta <- b_theta <- 0.5
 thres1 <- 0.1
 thres2 <- 0.8
 
-#scenario <- "stable"
+scenario <- "stable"
 #scenario <- "gradual"
-scenario <- "abrupt"
+#scenario <- "abrupt"
 
 
 if(scenario == "stable"){
